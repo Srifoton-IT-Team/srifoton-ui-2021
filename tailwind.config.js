0a882b214b642f2e14610ab8fb2cac4fefc0fab1
irvanmalik48 <irvanmalik48@gmail.com>
2021-07-01 17:34:42 +0700

    Revert prettify
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  purge: ["./index.html"],
+  purge: ['./index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         rubik: "'Rubik', sans-serif",
-        azonix: "'Azonix', 'Rubik', sans-serif",
+        azonix: "'Azonix', 'Rubik', sans-serif"
       },
       colors: {
         brand: {
           DEFAULT: "#108479",
           light: "#0f7a70",
-          dark: "#063430",
+          dark: "#063430"
         },
         cp: {
           DEFAULT: "#cbced3",
           light: "#eeeff0",
           dark: "#a9adb6",
-        },
-      },
+        }
+      }
     },
   },
   variants: {
     extend: {
-      backgroundColor: ["active"],
+      backgroundColor: ['active'],
     },
   },
   plugins: [],
-};
+}

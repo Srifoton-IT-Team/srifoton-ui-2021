976d9aaa7df0194a4d379049382cae5d4852e4f6
aryahmph <aryayun90@gmail.com>
2021-07-01 09:58:51 +0700

    Add variant color, and fix button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       fontFamily: {
         rubik: "'Rubik', sans-serif",
         azonix: "'Azonix', 'Rubik', sans-serif"
+      },
+      colors: {
+        brand: {
+          DEFAULT: "#108479",
+          light: "#0f7a70",
+          dark: "#063430"
+        },
+        cp: {
+          DEFAULT: "#cbced3",
+          light: "#eeeff0",
+          dark: "#a9adb6",
+        }
       }
     },
   },
